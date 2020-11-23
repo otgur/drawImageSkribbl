da9566440bfb516f1c5c5d220692fae90124e63c
Alexander Groß <contact@der-gross.de>
2020-11-23 22:18:49 +0100

    draw api for black only

---
+++ colors.py
@@ -0,0 +1,20 @@
+from enum import IntEnum
+
+def Color(IntEnum):
+    WHITE = 0
+    BLSCK = 1
+    LIGHTGRAY = 0
+    DARKGRAY = 3
+    RED = 4
+    DARKRED = 5
+    ORANGE = 6
+    DARKORANGE = 7
+    YELLOW = 8
+    DARKYELLOW = 9
+    GREEN = 10
+    DARKGREEN = 11
+    LIGHTBLUE = 12
+    MIDDLEBLUE = 13
+    BLUE = 14
+    DARKBLUE = 15
+    MORE = 16
